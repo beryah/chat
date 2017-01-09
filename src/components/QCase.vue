04a19353f2948fa9a92d70c973e2406c538e7cc8
Michael Mao <Michael Mao>
2017-01-10 01:52:15 +0800

    modal template

--- src/components/QCase.vue
+++ src/components/QCase.vue
@@ -1,18 +1,27 @@
 <template>
-	<div>
-		<ul>
-			<li v-for="qcase in this.$store.state.qcases">
-				<p>{{ qcase.issue[0].content.desc }}</p>
-				<p>Token: {{ qcase.token }}</p>
-				<p>SN: {{ qcase.initArg.SN }}</p>
-				<p>Email: {{ qcase.email }}</p>
-				<p>Phone: {{ qcase.phone }}</p>
-				<p>Birthday : {{ qcase.birthday  }}</p>
-				<p>Country : {{ qcase.country  }}</p>
-				<p>RequestDate : <span>{{getValue(qcase.issue[0].reporttime)}}<span></p>
-			</li>
-		</ul>
-	</div>	
+    <transition name="modal">
+        <div class="modal-mask">
+            <div class="modal-wrapper">
+                <div class="modal-container">
+                    <ul>
+                        <li v-for="qcase in this.$store.state.qcases">
+                            <p>{{ qcase.issue[0].content.desc }}</p>
+                            <p>Token: {{ qcase.token }}</p>
+                            <p>SN: {{ qcase.initArg.SN }}</p>
+                            <p>Email: {{ qcase.email }}</p>
+                            <p>Phone: {{ qcase.phone }}</p>
+                            <p>Birthday : {{ qcase.birthday }}</p>
+                            <p>Country : {{ qcase.country }}</p>
+                            <p>RequestDate : <span>{{getValue(qcase.issue[0].reporttime)}}<span></p>
+						</li>
+					</ul>
+					<button class="modal-default-button" @click="$emit('close')">
+		                OK
+		            </button>
+				</div>	
+			</div>
+		</div>		
+	</transition>
 </template>
 <script>
 export default {
@@ -23,3 +32,68 @@ export default {
 	}
 }
 </script>
+<style>
+.modal-mask {
+  position: fixed;
+  z-index: 99999;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+  transition: opacity .3s ease;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-container {
+  width: 300px;
+  margin: 0px auto;
+  padding: 20px 30px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
+  transition: all .3s ease;
+  font-family: Helvetica, Arial, sans-serif;
+}
+
+.modal-header h3 {
+  margin-top: 0;
+  color: #42b983;
+}
+
+.modal-body {
+  margin: 20px 0;
+}
+
+.modal-default-button {
+  float: right;
+}
+
+/*
+ * The following styles are auto-applied to elements with
+ * transition="modal" when their visibility is toggled
+ * by Vue.js.
+ *
+ * You can easily play with the modal transition by editing
+ * these styles.
+ */
+
+.modal-enter {
+  opacity: 0;
+}
+
+.modal-leave-active {
+  opacity: 0;
+}
+
+.modal-enter .modal-container,
+.modal-leave-active .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
+</style>
