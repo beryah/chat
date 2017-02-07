279370339d97cb55a384d30da6a4c23abdce2df4
michael <knicksmojs@gmail.com>
2017-02-07 18:35:12 +0800

    vue router test

--- src/main.js
+++ src/main.js
@@ -8,13 +8,47 @@ import 'jquery'
 import 'socket.io-client';
 import 'bootstrap/dist/js/bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
+import VueRouter from 'vue-router';
 import './css/chatmo.css';
 import moment from 'moment'
+
+Vue.use(VueRouter)
+
+const Home = { template: '<div>home</div>' }
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+
+
+
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    { path: '/', component: Home },
+    { path: '/foo', component: Foo },
+    { path: '/bar', component: Bar }
+  ]
+})
 
 /* eslint-disable no-new */
 
 new Vue({
   el: '#app',
   store,
-  render: h => h(App)
+  router,
+   template: `
+    <div id="app">
+      <h1>Basic</h1>
+      <ul>
+        <li><router-link to="/">/</router-link></li>
+        <li><router-link to="/foo">/foo</router-link></li>
+        <li><router-link to="/bar">/bar</router-link></li>
+        <router-link tag="li" to="/bar" :event="['mousedown', 'touchstart']">
+          <a>/bar</a>
+        </router-link>
+      </ul>
+      <router-view class="view"></router-view>
+    </div>
+  `
+  //render: h => h(App)
 })
